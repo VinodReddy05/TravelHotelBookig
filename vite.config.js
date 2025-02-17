4394954663550ac7f6e341ceebd791d7f8e16196
Janagam Vinod Reddy <jvinodreddy308@gmail.com>
2025-02-17 16:59:04 +0530

    firebase

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory as 'dist'
+  },
 })
